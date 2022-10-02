bcb61a0b68bb38beabdff32c6145580719f5d644
blade0220 <blade0220@yandex.ru>
2022-10-02 18:55:16 +0300

    Проверьте моё ДЗ_9 Объекты и классы

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,28 @@
 public class Book {
+    private String bookName;
+    int publishingYear;
+    Author author;
+
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    // public String getAuthor() {
+    //            return author.toString();
+    //  }
+
+    public int getPublishingYear() {
+        return publishingYear;
+    }
+
+    public Book(String bookName, Author author, int publishingYear) {
+        this.bookName = bookName;
+        // this.author = author;
+        this.publishingYear = publishingYear;
+    }
+
+    public void setPublishingYear(int publishingYear) {
+        this.publishingYear = publishingYear;
+    }
 }
